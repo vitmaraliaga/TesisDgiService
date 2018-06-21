00aa020768fcc1eaaf9b89377c40012ddece9e7c
valiaga <cruzjhonson@gmail.com>
2018-06-21 00:15:37 -0500

    Editar los asesores

--- apis/proyecto_api/views/asesor_view.py
+++ apis/proyecto_api/views/asesor_view.py
@@ -31,45 +31,35 @@ class AsesorSerializer(serializers.ModelSerializer):
             'fecha_creacion', 
             'fecha_actualizacion')
         extra_kwargs = {
-        'id': {
-            'read_only': False, 
-            'required': True
+            'persona.num_doc': {
+                'validators': [],
             }
-        } #very important
+        }
+
         read_only_fields = ('id', 'fecha_creacion', 'fecha_actualizacion',)
 
     def create(self, validated_data):
-        # persona_data = validated_data.pop('persona')
-        # asesor = Asesor.objects.create(**validated_data)
-        # Persona.objects.create(asesor=asesor, **persona_data)
-        # return asesor
         persona_data = validated_data.pop('persona')
-        persona = Persona.objects.create(**persona_data)
+        # persona = Persona.objects.create(**persona_data)
+        persona, created = Persona.objects.update_or_create(
+            pk=persona_data.get('id'),
+            defaults=persona_data)
         asesor = Asesor.objects.create(persona=persona, **validated_data)
         return asesor
 
     def update(self, instance, validated_data):
-        instance.persona = validated_data['persona']
+        persona_data = validated_data.get('persona')
+        instance.persona.nombres = persona_data.get('nombres', instance.persona.nombres)
+        instance.persona.apellido_paterno = persona_data.get('apellido_paterno', instance.persona.apellido_paterno)
+        instance.persona.apellido_materno = persona_data.get('apellido_materno', instance.persona.apellido_materno)
+        instance.persona.genero = persona_data.get('genero', instance.persona.genero)
+        instance.persona.fecha_nacimiento = persona_data.get('fecha_nacimiento', instance.persona.fecha_nacimiento)
+        instance.persona.num_doc = persona_data.get('num_doc', instance.persona.num_doc)
+        instance.persona.save()
+
+        instance.activo = validated_data.get('activo', instance.activo)
         instance.save()
-
-        # Delete any detail not included in the request
-        asesor_ids = [item['asesor_id'] for item in validated_data['asesors']]
-        for asesor in persona.asesors.all():
-            if asesor.id not in asesor_ids:
-                asesor.delete()
-
-        # Create or update owner 
-        for asesor in validated_data['asesors']:
-            asesorObj = Asesor.objects.get(pk=item['id'])
-            if asesorObje:
-                asesorObj.persona=item['persona']
-                ....fields...
-            else:
-               asesorObj = Asesor.create(car=instance,**owner)
-            ownerObj.save()
-
         return instance
-
 
 class AsesorViewSet(ModelPagination, viewsets.ModelViewSet):
     """
