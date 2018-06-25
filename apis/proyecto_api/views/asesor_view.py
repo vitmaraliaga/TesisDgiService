c99630272bcf6055397d3bece51775cd46914d78
valiaga <cruzjhonson@gmail.com>
2018-06-24 23:35:39 -0500

    Cambios pendientes

--- apis/proyecto_api/views/asesor_view.py
+++ apis/proyecto_api/views/asesor_view.py
@@ -42,15 +42,24 @@ class AsesorSerializer(serializers.ModelSerializer):
         asesor = Asesor.objects.create(persona=persona, **validated_data)
         return asesor
 
+# https://www.reddit.com/r/django/comments/6h6l2f/need_help_with_drf_serializer_validation/
     def update(self, instance, validated_data):
         persona_data = validated_data.get('persona')
-        instance.persona.nombres = persona_data.get('nombres', instance.persona.nombres)
-        instance.persona.apellido_paterno = persona_data.get('apellido_paterno', instance.persona.apellido_paterno)
-        instance.persona.apellido_materno = persona_data.get('apellido_materno', instance.persona.apellido_materno)
-        instance.persona.genero = persona_data.get('genero', instance.persona.genero)
-        instance.persona.fecha_nacimiento = persona_data.get('fecha_nacimiento', instance.persona.fecha_nacimiento)
-        instance.persona.num_doc = persona_data.get('num_doc', instance.persona.num_doc)
-        instance.persona.save()
+        ipersona = Persona.objects.get(id=instance.persona.id)
+        ipersona.nombres = persona_data.get('nombres', ipersona.nombres)
+        ipersona.apellido_paterno = persona_data.get('apellido_paterno', ipersona.apellido_paterno)
+        ipersona.apellido_materno = persona_data.get('apellido_materno', ipersona.apellido_materno)
+        ipersona.genero = persona_data.get('genero', ipersona.genero)
+        ipersona.fecha_nacimiento = persona_data.get('fecha_nacimiento', ipersona.fecha_nacimiento)
+        ipersona.num_doc = persona_data.get('num_doc', ipersona.num_doc)
+        ipersona.persona.save()
+        # instance.persona.nombres = persona_data.get('nombres', instance.persona.nombres)
+        # instance.persona.apellido_paterno = persona_data.get('apellido_paterno', instance.persona.apellido_paterno)
+        # instance.persona.apellido_materno = persona_data.get('apellido_materno', instance.persona.apellido_materno)
+        # instance.persona.genero = persona_data.get('genero', instance.persona.genero)
+        # instance.persona.fecha_nacimiento = persona_data.get('fecha_nacimiento', instance.persona.fecha_nacimiento)
+        # instance.persona.num_doc = persona_data.get('num_doc', instance.persona.num_doc)
+        # instance.persona.save()
 
         instance.activo = validated_data.get('activo', instance.activo)
         instance.save()
